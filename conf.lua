9fd0466f6543e7f42d537a7d0d1c66be8e163a14
banepwn <64751338+banepwn@users.noreply.github.com>
2021-01-21 10:05:08 -0500

    avoid loading unnecessary modules

--- conf.lua
+++ conf.lua
@@ -2,4 +2,24 @@ function love.conf(t)
 	t.window.title = "WBUI demo"
 	t.window.width = 1024
 	t.window.height = 768
+	t.modules = {
+		audio = false,
+		data = false,
+		event = true,
+		font = true,
+		graphics = true,
+		image = true,
+		joystick = false,
+		keyboard = true,
+		math = false,
+		mouse = true,
+		physics = false,
+		sound = false,
+		system = true,
+		thread = false,
+		timer = true,
+		touch = true,
+		video = false,
+		window = true
+	}
 end
